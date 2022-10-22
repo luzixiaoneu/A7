921061cf622810662a5374745b8bcbe3e1f71121
luzixiaoneu <lu.zix@northeastern.edu>
2022-10-21 21:45:39 -0400

    add button

--- app/src/main/java/com/example/spotify_test1/MainActivity.java
+++ app/src/main/java/com/example/spotify_test1/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
@@ -20,7 +22,7 @@ import com.spotify.sdk.android.auth.AuthorizationResponse;
 
 public class MainActivity extends AppCompatActivity {
     private static final String CLIENT_ID = "78698dcc31fd4845919e96fc61514de8";
-    private static final String REDIRECT_URI = "http://localhost:8888/callback/";
+    private static final String REDIRECT_URI = "http://localhost:8888/";
     private SpotifyAppRemote mSpotifyAppRemote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spotifyLogin();
+            }
+        });
+    }
+
+    private void spotifyLogin() {
         AuthorizationRequest.Builder builder =
                 new AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI);
 
@@ -37,82 +49,34 @@ public class MainActivity extends AppCompatActivity {
         AuthorizationClient.openLoginActivity(this, 1337, request);
     }
 
+
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
         // Check if result comes from the correct activity
         if (requestCode == 1337) {
             AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, intent);
-
+            Log.d("result", "onActivityResult: ");
             switch (response.getType()) {
                 // Response was successful and contains auth token
                 case TOKEN:
                     // Handle successful response
+                    Log.d("good ", response.getAccessToken());
                     break;
 
                 // Auth flow returned an error
                 case ERROR:
+                    Log.d("error ", response.getError());
                     // Handle error response
                     break;
 
                 // Most likely auth flow was cancelled
                 default:
+                    Log.d("wtf ", response.getType().toString());
                     // Handle other cases
             }
         }
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // We will start writing our code here.
-        ConnectionParams connectionParams =
-                new ConnectionParams.Builder(CLIENT_ID)
-                        .setRedirectUri(REDIRECT_URI)
-                        .showAuthView(true)
-                        .build();
 
-        connectionParams.shouldShowAuthView();
-
-        SpotifyAppRemote.connect(this, connectionParams,
-                new Connector.ConnectionListener() {
-
-                    @Override
-                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                        mSpotifyAppRemote = spotifyAppRemote;
-                        Log.d("MainActivity", "Connected! Yay!");
-
-                        // Now you can start interacting with App Remote
-                        connected();
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        Log.e("MainActivity", throwable.getMessage(), throwable);
-
-                        // Something went wrong when attempting to connect! Handle errors here
-                    }
-                });
-    }
-
-    private void connected() {
-        // Then we will write some more code here.
-        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
-        mSpotifyAppRemote.getPlayerApi()
-                .subscribeToPlayerState()
-                .setEventCallback(playerState -> {
-                    final Track track = playerState.track;
-                    if (track != null) {
-                        Log.d("MainActivity", track.name + " by " + track.artist.name);
-                    }
-                });
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
-        // Aaand we will finish off here.
-    }
 }
